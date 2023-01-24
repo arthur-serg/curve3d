4a52a2080e71c7dc56d5c03b2cf83f16ddf51317
exponenta.euler <exponenta.euler@gmail.com>
2023-01-24 15:26:04 +0500

    virtual destructor and name()

--- Curves3D/Curve.hpp
+++ Curves3D/Curve.hpp
@@ -1,11 +1,13 @@
 #pragma once
+#include <iostream>
 #include "Point.hpp"
-
 namespace curve {
 
 class Curve {
  public:
+  virtual std::string getName() const = 0;
   virtual Point getValue(const double& t) const = 0;
   virtual Point getDerivative(const double& t) const = 0;
+  virtual ~Curve() { std::cout << "~Curve()" << std::endl; }
 };
 }  // namespace curve
