f24e26b7aa5bb076658557135c6556dea82585e8
exponenta.euler <exponenta.euler@gmail.com>
2023-01-24 15:25:26 +0500

    ellipse implementation

--- Curves3D/Ellipse.cpp
+++ Curves3D/Ellipse.cpp
@@ -1,4 +1,21 @@
 #include "Ellipse.hpp"
 
-
-namespace curve {}
+namespace curve {
+Ellipse::Ellipse(const double& a, const double& b, const double& t)
+    : a(a), b(b), t(t) {
+  std::cout << "Ellipse::Ellipse(a,b,t)" << std::endl;
+}
+std::string Ellipse::getName() const {
+  std::cout << "Ellipse" << std::endl;
+  return "Ellipse";
+}
+Point Ellipse::getValue(const double& t) const {
+  return Point(a * cos(t), b * sin(t), 0);
+}
+Point Ellipse::getDerivative(const double& t) const {
+  return Point(-a * sin(t), b * cos(t), 0);
+}
+Ellipse::~Ellipse() {
+  std::cout << "~Ellipse()" << std::endl;
+}
+}  // namespace curve
--- Curves3D/Ellipse.hpp
+++ Curves3D/Ellipse.hpp
@@ -1,3 +1,21 @@
 #pragma once
+#include <math.h>
+#include "Curve.hpp"
+namespace curve {
 
-namespace curve {}
+class Ellipse : public Curve {
+ public:
+  Ellipse(const double& a, const double& b, const double& t);
+  std::string getName() const override;
+  Point getValue(const double& t) const override;
+  Point getDerivative(const double& t) const override;
+  virtual ~Ellipse();
+
+ private:
+  //Ellipse* ellipse;
+  double a;
+  double b;
+  double t;
+};
+
+}  // namespace curve
