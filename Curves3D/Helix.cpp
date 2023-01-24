3bb2fba6859a4af351d2d69125b3d9ebcfdd986a
exponenta.euler <exponenta.euler@gmail.com>
2023-01-24 15:25:41 +0500

    helix implementation

--- Curves3D/Helix.cpp
+++ Curves3D/Helix.cpp
@@ -1,4 +1,26 @@
 #include "Helix.hpp"
 
+namespace curve {
+Helix::Helix(const double& radius, const double& step, const double& t)
+    : radius(radius), step(step), t(t) {
+  std::cout << "Helix::Helix(a,b,t)" << std::endl;
+}
 
-namespace curve {}
+std::string Helix::getName() const {
+  std::cout << "Helix" << std::endl;
+  return "Helix";
+}
+
+Point Helix::getValue(const double& t) const {
+  return Point(radius * cos(t), radius * sin(t), step * t);
+}
+
+Point Helix::getDerivative(const double& t) const {
+  return Point(-radius * sin(t), radius * cos(t), step);
+}
+
+Helix::~Helix() {
+  std::cout << "~Helix()" << std::endl;
+}
+
+}  // namespace curve
--- Curves3D/Helix.hpp
+++ Curves3D/Helix.hpp
@@ -1,3 +1,17 @@
 #pragma once
+#include "Curve.hpp"
+namespace curve {
+class Helix : public Curve {
+ public:
+  Helix(const double& radius, const double& step, const double& t);
+  std::string getName() const override;
+  Point getValue(const double& t) const override;
+  Point getDerivative(const double& t) const override;
+  virtual ~Helix();
 
-namespace curve{}
+ private:
+  double radius;
+  double step;
+  double t;
+};
+}  // namespace curve
