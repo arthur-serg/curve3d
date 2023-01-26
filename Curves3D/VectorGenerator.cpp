ddc13f449da656f314c5a55b707b8c620b4c72d2
exponenta.euler <exponenta.euler@gmail.com>
2023-01-26 16:00:17 +0500

    generate std::vector of curves

--- Curves3D/VectorGenerator.cpp
+++ Curves3D/VectorGenerator.cpp
@@ -1,7 +1,15 @@
 #include "VectorGenerator.hpp"
 #include <random>
-#include <vector>
+
 namespace curve {
 
-class VectorGenerator {};
+VectorGenerator::VectorGenerator(const int& upperBound) {
+  std::cout << "VectorGenerator::VectorGenerator()" << std::endl;
+  std::random_device randomizer;
+  std::mt19937 generator(randomizer());
+  std::uniform_int_distribution<> distribution(1, upperBound);
+  size = distribution(generator);
+  data.resize(size);
+  std::cout << "size: " << size << std::endl;
+}
 }  // namespace curve
--- Curves3D/VectorGenerator.hpp
+++ Curves3D/VectorGenerator.hpp
@@ -1,2 +1,16 @@
 #pragma once
-namespace curve {}
+#include <vector>
+#include "Curve.hpp"
+namespace curve {
+class VectorGenerator {
+  typedef std::unique_ptr<Curve> CurvePtr;
+
+ public:
+  explicit VectorGenerator(const int& upperBound);
+  ~VectorGenerator() = default;
+
+ private:
+  std::vector<CurvePtr> data;
+  int size;
+};
+}  // namespace curve
