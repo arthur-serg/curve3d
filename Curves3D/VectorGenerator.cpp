7045180119f46612a309522f53505bc68d860750
exponenta.euler <exponenta.euler@gmail.com>
2023-01-27 14:53:19 +0500

    generate std::vector of random curves. print data, derivatives wrt to t

--- Curves3D/VectorGenerator.cpp
+++ Curves3D/VectorGenerator.cpp
@@ -3,13 +3,36 @@
 
 namespace curve {
 
-VectorGenerator::VectorGenerator(const int& upperBound) {
-  std::cout << "VectorGenerator::VectorGenerator()" << std::endl;
-  std::random_device randomizer;
-  std::mt19937 generator(randomizer());
-  std::uniform_int_distribution<> distribution(1, upperBound);
-  size = distribution(generator);
-  data.resize(size);
-  std::cout << "size: " << size << std::endl;
+VectorGenerator::VectorGenerator(const int& size) {
+  for (int i = 0; i < size; ++i) {
+    int trigger = RandomDouble(3).getValue();
+    switch (trigger) {
+      case 0:
+        data.emplace_back(std::move(new Circle));
+        break;
+      case 1:
+        data.emplace_back(std::move(new Ellipse));
+        break;
+      case 2:
+        data.emplace_back(std::move(new Helix));
+        break;
+    }
+  }
+}
+void VectorGenerator::printValues(const double& t) {
+  if (!data.empty()) {
+    for (const auto& element : data) {
+      std::cout << element.get()->getName() << " at t=" << t << std::endl;
+      element.get()->getValue(t).print();
+    }
+  }
+}
+void VectorGenerator::printDerivatives(const double& t) {
+  if (!data.empty()) {
+    for (const auto& element : data) {
+      std::cout << element.get()->getName() << " at t=" << t << std::endl;
+      element.get()->getDerivative(t).print();
+    }
+  }
 }
 }  // namespace curve
--- Curves3D/VectorGenerator.hpp
+++ Curves3D/VectorGenerator.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include <vector>
+#include "Circle.hpp"
 #include "Curve.hpp"
+#include "Ellipse.hpp"
+#include "Helix.hpp"
 namespace curve {
 class VectorGenerator {
-  typedef std::unique_ptr<Curve> CurvePtr;
+  typedef std::shared_ptr<Curve> CurvePtr;
 
  public:
-  explicit VectorGenerator(const int& upperBound);
+  explicit VectorGenerator(const int& size);
   ~VectorGenerator() = default;
+  void printValues(const double& t);
+  void printDerivatives(const double& t);
 
  private:
   std::vector<CurvePtr> data;
